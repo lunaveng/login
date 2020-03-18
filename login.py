dc5caef11f4ab70556bab77f49271385eb2a2dd5
lunaveng <62343300+lunaveng@users.noreply.github.com>
2020-03-18 09:36:36 -0600

    Update login.py

---
+++ login.py
@@ -0,0 +1,18 @@
+from tkinter import *
+ventana=Tk()
+ventana.config(bg="#3cb371")
+miFrame = Frame(ventana, width=200, heigh=200)
+heigth=200
+miFrame.pack()
+la1= Label(miFrame, text ="usuario")
+la1.grid(row=0, column=0, pady=10,padx=10)
+la2= Label(miFrame, text="password")
+la2.grid(row=1, column=0, pady=10,padx=10)
+txt1 =Entry(miFrame,justify="center")
+txt1.grid(row=0, column=1, pady=10,padx=10)
+txt2 =Entry(miFrame, show="*")
+txt2.grid(row=1, column=1, pady=10,padx=10)
+btn= Button(miFrame,text="aceptar" )
+btn.grid(row=2, column=0, pady=10,padx=10,columnspan=2)
+btn.config(width=30)
+ventana.mainloop()
